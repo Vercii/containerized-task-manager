b7dfdf4b1de6720c9b0f1464086aa032e72c2ba8
Renzo Takeda <renzoisaac912@gmail.com>
2026-06-16 10:41:21 +0800

    CRUD Functions

--- app.js
+++ app.js
@@ -13,12 +13,63 @@ let tasks = [
 
 app.use(express.json());
 
+// Routes
+// GET
 app.get("/", (req, res) => {
     res.send("Task Manager is running!");
 });
 
 app.get("/tasks", (req, res) => {
     res.json(tasks);
+});
+
+// POST
+app.post("/tasks", (req, res) => {
+    const newTask = {
+        id: tasks.length + 1,
+        title: req.body.title,
+        completed: false
+    };
+
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+});
+
+// PUT (UPDATING)
+app.put("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const task = tasks.find(t => t.id === id);
+
+    if (!task) {
+        return res.status(404).json({
+            message: "Task not found"
+        });
+    }
+
+    task.completed = req.body.completed;
+
+    res.json(task);
+});
+
+// DELETE
+app.delete("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const taskExists = tasks.some(task => task.id === id);
+
+    if (!taskExists) {
+        return res.status(404).json({
+            message: "Task not found"
+        });
+    }
+
+    tasks = tasks.filter(task => task.id !== id);
+
+    res.json({
+        message: "Task deleted"
+    });
 });
 
 app.listen(PORT, () => {
